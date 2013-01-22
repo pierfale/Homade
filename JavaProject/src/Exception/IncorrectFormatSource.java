6ec4764abec6741d1a884cdddfdf0f7eb4d335b4
pierre falez <pierfale@hotmail.fr>
2013-01-22 17:37:28 +0100

    Ajout de la traduction de l'instruction assembleur ip vers le binaire Ajout de l'exception IncorrectFormatSource qui sert a désigner une erreur dans le code source
    Aigghtt :D

---
+++ JavaProject/src/Exception/IncorrectFormatSource.java
@@ -0,0 +1,17 @@
+package Exception;
+
+public class IncorrectFormatSource extends Exception {
+	
+	private String message;
+	private int line;
+	
+	public IncorrectFormatSource(String message, int line) {
+		this.message = message;
+		this.line = line;
+	}
+	
+	public String toString() {
+		return new String("Code source incorrecte : "+message+" à la ligne "+line);
+	}
+
+}
