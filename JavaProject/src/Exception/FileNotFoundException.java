7a6f9c0449dd2e0169e659fb93f5aa57e630c329
pierre falez <pierfale@hotmail.fr>
2013-01-23 20:14:03 +0100

    Ajout d'un premier jet sur le processeur et le core Debug de la methode pour compiler Commencement de la methode de décompilation

---
+++ JavaProject/src/Exception/FileNotFoundException.java
@@ -0,0 +1,16 @@
+package Exception;
+
+@SuppressWarnings("serial")
+public class FileNotFoundException extends Exception {
+	
+	private String message;
+	
+	public FileNotFoundException(String message) {
+		this.message = message;
+	}
+	
+	public String toString() {
+		return new String("Fichier non trouver : "+message);
+	}
+
+}
---
+++ JavaProject/src/Processor/Processor.java
@@ -0,0 +1,74 @@
+package Processor;
+
+/**
+ * 
+ * @author falezp
+ * 
+ * Singleton qui représente le processeur
+ *
+ */
+
+public class Processor {
+	
+	private static boolean on;
+	private static long [] instructions;
+	private static int PC; //compteur ordinal
+	private static int regAddr;
+	private static Core core;
+	
+	
+	/**
+	 * Singleton pattern
+	 */
+	private Processor() {
+		
+	}
+	
+	/**
+	 * Initialisation du processeur :
+	 * creation du coeur
+	 * Cette fonction doit etre appeler avant tout appele de la fonction start
+	 */
+	public static void init() {
+		core = new Core();
+	}
+	
+	
+	public static void start(long [] instr) {
+		on = true;
+		instructions = instr;
+		PC = 0;
+		while(on) {
+			pulse();
+		}
+	}
+	
+	public static void stop() {
+		on = false;
+	}
+	
+
+	/**
+	 * fonction appeler a chaque pulsation du processeur
+	 */
+	private static void pulse() {
+		core.exec(fetch());
+		
+		
+	}
+	
+	private static short fetch() {
+		short currInstr = (short) (instructions[PC/4] >> 16*(3 - PC%4));
+		System.out.println(String.format("%x", currInstr));
+		if(((currInstr >> 13) & 0x1) == 0xF || ((currInstr >> 9) & 0x3) == 0x3F) { //si call ou ba
+			System.out.println("regAddr");
+			PC++;
+			short currInstr2 = (short) (instructions[PC/4] >> 16*(3 - PC%4));
+			regAddr = currInstr2 << 16;
+			PC++;
+			regAddr += (short) (instructions[PC/4] >> 16*(3 - PC%4));
+		}
+		PC++;
+		return currInstr;
+	}
+}
